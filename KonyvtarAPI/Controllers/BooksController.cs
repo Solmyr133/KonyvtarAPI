c870f91162e7bb9ef06e8a5030800ad52b6a4aa8
Solmyr133 <solmyr13369@gmail.com>
2025-02-10 08:08:55 +0100

    Created GET all endpoint

--- KonyvtarAPI/Controllers/BooksController.cs
+++ KonyvtarAPI/Controllers/BooksController.cs
@@ -10,6 +10,51 @@ namespace KonyvtarAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly LibraryContext _context;
 
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Book>> GetBooks()
+        {
+            var books = await _context.Books.ToListAsync();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBook(CreateBookDTO createBookDTO)
+        {
+            var currentBook = new Book
+            {
+                Title = createBookDTO.Title,
+                Author = createBookDTO.Author,
+                PublishedYear = createBookDTO.PublishedYear,
+                Genre = createBookDTO.Genre,
+                Price = createBookDTO.Price,
+            };
+
+            _context.Books.Add(currentBook);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBookById), new { id = currentBook.Id }, currentBook);
+        }
     }
 }
